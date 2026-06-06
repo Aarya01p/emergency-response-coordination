4af582d00429ad224ec172533042fadec67ced14
Aarya Patel <aaryap066@gmail.com>
2026-06-06 22:25:29 +0530

    Update api.ts

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_URL =
+  process.env.REACT_APP_API_URL ||
+  'https://emergency-response-backend-n8xb.onrender.com/api';
 
 export const api = axios.create({
   baseURL: API_URL,
